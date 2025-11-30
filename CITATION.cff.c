f28407a5f359f973ad02895be55e0bf929cca07c
averycheng-Hulab <averycheng.peanut@gmail.com>
2025-12-01 04:43:31 +0800

    Add files via upload

---
+++ CITATION.cff.c
@@ -0,0 +1,11 @@
+cff-version: 1.2.0
+message: "If you use this project, please cite it using the metadata below."
+title: "XXXX"
+authors:
+  - family-names: "Cheng"
+    given-names: "Guo"
+version: "1.0.0"
+date-released: "2025-12-01"
+doi: ""
+url: "https://github.com/averycheng-Hulab/iCDC"
+license: "MIT"
