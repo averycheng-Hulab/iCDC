b6469270a5bd7d49473b4da11fefa582103c39e0
averycheng-Hulab <averycheng.peanut@gmail.com>
2026-02-05 18:12:18 +0800

    Update bulk_3.R

--- bulk/bulk_3.R
+++ bulk/bulk_3.R
@@ -31,7 +31,7 @@ suppressPackageStartupMessages({
 })
 
 # Source shared utilities
-source(file.path("bulk", "utils_bulk.R"))
+source(file.path("bulk","utils","utils_bulk.R"))
 
 ############################################################
 # 1. User settings
